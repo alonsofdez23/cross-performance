3c7d45965a800bc96d8a92a3dfe3a83a63b9db94
alonsofdez23 <alonsobs23@gmail.com>
2022-11-14 12:05:59 +0100

    Modificado redirect al loguear

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const CLASES = '/clases';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
