64377cfa8bb8850f1c74758091fa957098651074
alonsofdez23 <alonsobs23@gmail.com>
2022-11-27 21:46:05 +0100

    Formulario clases v2

--- app/Http/Livewire/Admin/AdminHorario.php
+++ app/Http/Livewire/Admin/AdminHorario.php
@@ -3,27 +3,51 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Clase;
+use App\Models\User;
 use Carbon\Carbon;
 use Livewire\Component;
 use Label84\HoursHelper\Facades\HoursHelper;
 
 class AdminHorario extends Component
 {
-    public $hora;
+    public $horaInicio;
+    public $horaFin;
+    public $duracion;
+    public $dia;
+
     public $monitor;
     public $vacantes;
 
     public function submit()
     {
-        Clase::create([
+        $horario = HoursHelper::create(
+            $this->dia . ' ' . $this->horaInicio,
+            $this->dia . ' ' . $this->horaFin,
+            $this->duracion,
+            'Y-m-d H:i',
+        );
+
+        foreach ($horario as $clase) {
+            Clase::create([
+                'monitor_id' => $this->monitor,
+                'fecha_hora' => Carbon::createFromTimeString($clase)->subHour(),
+                'vacantes' => $this->vacantes,
+            ]);
+        }
+
+        /* Clase::create([
             'monitor_id' => $this->monitor,
             'fecha_hora' => Carbon::createFromTimeString($this->hora),
             'vacantes' => $this->vacantes,
-        ]);
+        ]); */
     }
 
     public function render()
     {
-        return view('livewire.admin.admin-horario');
+        $monitores = User::role('admin')->get();
+
+        return view('livewire.admin.admin-horario', [
+            'monitores' => $monitores,
+        ]);
     }
 }
--- resources/views/livewire/admin/admin-horario.blade.php
+++ resources/views/livewire/admin/admin-horario.blade.php
@@ -5,21 +5,51 @@
                 <form wire:submit.prevent="submit">
 
                     <div>
-                        <x-jet-label for="hora" value="Hora" />
-                        <x-jet-input wire:model="hora" id="hora" type="text" class="mt-1 block w-full" />
-                        <x-jet-input-error for="hora" class="mt-2" />
+                        <x-jet-label for="dia" value="Día" />
+                        <x-jet-input wire:model="dia" id="dia" type="text" placeholder="2022-11-27" class="mt-1 block w-full" />
+                        <x-jet-input-error for="dia" class="mt-2" />
                     </div>
 
                     <div>
-                        <x-jet-label for="monitor" value="Monitor" />
-                        <x-jet-input wire:model="monitor" id="monitor" type="text" class="mt-1 block w-full" />
-                        <x-jet-input-error for="monitor" class="mt-2" />
+                        <x-jet-label for="hora_inicio" value="Hora de apertura" />
+                        <x-jet-input wire:model="horaInicio" id="hora_inicio" type="text" placeholder="8:00" class="mt-1 block w-full" />
+                        <x-jet-input-error for="hora_inicio" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-jet-label for="hora_fin" value="Hora de cierre" />
+                        <x-jet-input wire:model="horaFin" id="hora_fin" type="text" placeholder="15:00" class="mt-1 block w-full" />
+                        <x-jet-input-error for="hora_fin" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-jet-label for="duracion" value="Duración de clases (minutos)" />
+                        <x-jet-input wire:model="duracion" id="duracion" type="number" placeholder="60" class="mt-1 block w-full" />
+                        <x-jet-input-error for="duracion" class="mt-2" />
                     </div>
 
                     <div>
                         <x-jet-label for="vacantes" value="Plazas disponibles" />
-                        <x-jet-input wire:model="vacantes" id="vacantes" type="number" class="mt-1 block w-full" />
+                        <x-jet-input wire:model="vacantes" id="vacantes" type="number" placeholder="10" class="mt-1 block w-full" />
                         <x-jet-input-error for="vacantes" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-jet-label for="monitor" value="Monitor" />
+                        <select wire:model="monitor" name="monitor" id="monitor" class="mt-1 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
+                            <option value="">Selecciona monitor</option>
+                            @foreach ($monitores as $monitor)
+                                <option value="{{ $monitor->id }}">{{ $monitor->name }}</option>
+                            @endforeach
+                        </select>
+                        {{-- <x-select
+                            placeholder="Selecciona monitor"
+                            wire:model.defer="monitor"
+                            >
+                            @foreach ($monitores as $monitor)
+                                <x-select.user-option src="{{ $monitor->profile_photo_url }}" label="{{ $monitor->name }}" value="{{ $monitor->id }}" />
+                            @endforeach
+                        </x-select> --}}
                     </div>
 
             </div>
