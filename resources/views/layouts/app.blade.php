6f17193e78235b1385276968791e59c54b41a030
alonsofdez23 <alonsobs23@gmail.com>
2022-12-08 22:23:16 +0100

    Recoge datos tarjeta y resetea con spinner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,16 @@
 
         <!-- Scripts -->
         @wireUiScripts
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite([
+            'resources/css/app.css',
+            'resources/css/forms.css',
+            'resources/css/spinner.css',
+            'resources/js/app.js'])
+        <script src="https://js.stripe.com/v3/"></script>
+
+        @isset($css)
+            {{ $css }}
+        @endisset
 
         <!-- Styles -->
         @livewireStyles
@@ -43,5 +52,9 @@
 
         @livewireScripts
         @yield('scripts')
+
+        @isset($js)
+            {{ $js }}
+        @endisset
     </body>
 </html>
