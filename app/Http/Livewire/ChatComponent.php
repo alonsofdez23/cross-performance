28fe724d212c5a491aecc3dc8e2215fbbbe74a7a
alonsofdez23 <alonsobs23@gmail.com>
2023-01-04 23:48:11 +0100

    Lista contactos ajax

--- app/Http/Livewire/ChatComponent.php
+++ app/Http/Livewire/ChatComponent.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class ChatComponent extends Component
 {
+    public $search;
+
+    // Propiedad computada
+    public function getUsersProperty()
+    {
+        return User::when($this->search, function($query){
+            $query->where(function($query){
+                $query->where('name', 'like', '%'.$this->search.'%')
+                    ->orWhere('email', 'like', '%'.$this->search.'%');
+            });
+        })->get();
+    }
+
     public function render()
     {
-        return view('livewire.chat-component');
+        return view('livewire.chat-component')->layout('layouts.chat');
     }
 }
--- resources/views/livewire/chat-component.blade.php
+++ resources/views/livewire/chat-component.blade.php
@@ -11,11 +11,37 @@
 
                         <div class="bg-gray-200 h-14 flex items-center px-4">
                             <x-jet-input type="text"
+                                wire:model="search"
                                 class="w-full"
                                 placeholder="Buscar" />
                         </div>
                         <div class="h-[calc(100vh-18.5rem)] overflow-auto border-t border-gray-700">
+                            <div class="px-4 py-3">
+                                <h2 class="text-gray-700 text-lg mb-4">Contactos</h2>
 
+                                <ul class="space-y-4">
+                                    @forelse ($this->users as $user)
+                                        <li class="cursor-pointer">
+                                            <div class="flex">
+                                                <figure class="flex-shrink-0">
+                                                    <img class="h-12 w-12 object-cover object-center rounded-full" src="{{ $user->profile_photo_url }}">
+                                                </figure>
+
+                                                <div class="flex-1 ml-5 border-b border-gray-700">
+                                                    <p class="text-gray-700">
+                                                        {{ $user->name }}
+                                                    </p>
+                                                    <p class="text-gray-500 text-xs">
+                                                        {{ $user->email }}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    @empty
+
+                                    @endforelse
+                                </ul>
+                            </div>
                         </div>
                     </div>
                     <div class="col-span-2">
