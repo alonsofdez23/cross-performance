b65c8fbb3670a7156c9360f2167b8693d1eaec7a
alonsofdez23 <alonsobs23@gmail.com>
2023-02-02 15:52:54 +0100

    Logo plantilla mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,9 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@if (trim($slot) === 'CrossPerformance')
+<img src="https://www.svgrepo.com/show/314923/dumbbell.svg" class="logo" alt="CrossPerformance">
+{{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
 @else
 {{ $slot }}
 @endif
